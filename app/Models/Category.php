17b1cc7753ca8321b8ab9317d0c1c1f4a119f01b
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-10-23 23:16:57 -0300

    feat: add casts and friendly slugs

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\SlugOptions;   
 
 
 class Category extends Model
@@ -13,7 +14,18 @@ class Category extends Model
     protected $fillable = [
         'name',
         'description',
-    ];  
+        'slug',
+    ];
+
+    protected $casts = [
+        'description' => 'array',
+    ];
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 
 
     public function products(): HasMany
