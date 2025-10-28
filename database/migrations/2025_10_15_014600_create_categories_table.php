ef35d59b435f8b0c613858be59548d753b1c5085
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-10-27 21:31:13 -0300

    feat: add unique constraint to category name column

--- database/migrations/2025_10_15_014600_create_categories_table.php
+++ database/migrations/2025_10_15_014600_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->index();
+            $table->string('name')->unique()->index();
             $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->boolean('is_active')->default(true);
