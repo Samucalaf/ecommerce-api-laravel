3486291634a7abeb1026ffbd545a25f724d8ad07
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-10-31 09:13:51 -0300

    feat: add seeder and factory to generate fake category and product data for tests

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class ProductSeeder extends Seeder
 {
@@ -12,12 +13,9 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::create([
-            'name' => 'Sample Product',
-            'description' => 'This is a sample product.',
-            'price' => 99.99,
-            'stock' => 50,
-            'category_id' => 1,
+        $categories = Category::all();
+        Product::factory()->count(10)->create([
+            'category_id' => $categories->random()->id,
         ]);
     }
 }
