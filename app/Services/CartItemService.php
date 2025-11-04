82be81123a4514656dfee2299afaa75958039af2
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-11-04 16:24:37 -0300

    feat: add service for cart items CRUD operations (store, update and delete)

---
+++ app/Services/CartItemService.php
@@ -0,0 +1,75 @@
+<?php 
+
+namespace App\Services;
+use App\Models\Product;
+
+class CartItemService
+{
+    protected $cartItemRepository;
+
+    public function __construct($cartItemRepository)
+    {
+        $this->cartItemRepository = $cartItemRepository;
+    }
+
+        public function addProductToCart($idUser, $productId, $quantity)
+    {
+        $product = Product::find($productId);
+        if (!$product ) {
+            throw new \InvalidArgumentException("Invalid product ID.");
+        }
+
+        if ($product->is_active === false){
+            throw new \InvalidArgumentException("Product is inactive.");
+        }
+
+        if ($product->stock < $quantity) {
+            throw new \InvalidArgumentException("Insufficient stock for the product.");
+        }
+
+        if (!is_int($quantity)) {
+            throw new \InvalidArgumentException("Quantity must be an integer.");
+        }
+        if ($quantity <= 0) {
+            throw new \InvalidArgumentException("Quantity must be greater than zero.");
+        }
+
+        return $this->cartItemRepository->postProductToCart($idUser, $productId, $quantity);
+    }
+
+        public function updateProductInCart($userId, $productId, $quantity)
+    {
+        $product = Product::find($productId);
+        if (!$product) {
+            throw new \InvalidArgumentException("Product does not exist in cart.");
+        }
+
+        if ($product->is_active === false){
+            throw new \InvalidArgumentException("Product is inactive.");
+        }
+
+        if ($quantity <= 0) {
+            return $this->cartItemRepository->removeProduct($userId, $product);
+        }
+        if ($product->stock < $quantity) {
+            throw new \InvalidArgumentException("Insufficient stock for the product.");
+        }
+        return $this->cartItemRepository->updateProduct($userId, $product, $quantity);
+    }
+
+        public function removeProductFromCart($userId, $productId)
+    {
+        $product = Product::find($productId);
+
+        if (!$product) {
+            throw new \InvalidArgumentException("Product does not exist in cart");
+        }
+        if ($product->is_active === false){
+            throw new \InvalidArgumentException("Product is inactive.");
+        }
+
+        return $this->cartItemRepository->removeProduct($userId, $product);
+    }
+
+
+}
