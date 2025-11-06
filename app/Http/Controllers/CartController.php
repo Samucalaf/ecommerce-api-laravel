58275c84096cb25fe139b688c064938544dee361
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-11-06 13:42:57 -0300

    feat: implement controller to handle cart view and clear actions

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\CartService;
+class CartController extends Controller
+{
+    protected $cartService;
+
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $userId = $request->user()->id;
+        $cart = $this->cartService->showCartUser($userId);
+        return response()->json($cart);
+    }
+
+    public function clear(Request $request)
+    {
+        $userId = $request->user()->id;
+        $this->cartService->clearUserCart($userId);
+        return response()->json(['message' => 'Cart cleared successfully.']);
+    }
+}
