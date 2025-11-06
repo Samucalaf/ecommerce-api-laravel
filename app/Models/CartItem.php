c6ee4950e688919cdbde037fb5214e9be81ecd48
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-11-05 23:07:41 -0300

    feat: add method for calculate the total price

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class CartItem extends Model
 {
     protected $fillable = [
         'cart_id',
         'product_id',
         'quantity',
+        'price',
     ];
 
     public function cart(): BelongsTo
@@ -20,5 +22,9 @@ class CartItem extends Model
     public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
+    }
+    public function calculateTotal()
+    {
+        return $this->quantity * $this->price;
     }
 }
