2845fe2ddc8fb4144dd343977d4401cc9e11f0c6
Samuel Lafuente Coelho <samuellafuent955@gmail.com>
2025-11-16 20:57:14 -0300

    feat: add controller for address management

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Addresse\StoreAddresseRequest;
+use App\Http\Requests\Addresse\UpdateAddresseRequest;
+use App\Http\Resources\AddressResource;
+use App\Http\Resources\AddressCompleteResource;
+use Illuminate\Http\Request;
+use App\Services\AddresseService;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    protected $addresseService;
+
+    public function __construct(AddresseService $addresseService)
+    {
+        $this->addresseService = $addresseService;
+    }
+    
+    public function index(Request $request)
+    {
+        $user = $request->user();
+        $addresses = $this->addresseService->listUserAddresses($user->id);
+        
+        return response()->json([
+            'data' => AddressResource::collection($addresses)
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAddresseRequest $request)
+    {
+        $dataValidated = $request->validated();
+        $address = $this->addresseService->createAddress($dataValidated);
+        
+        return response()->json([
+            'message' => 'Address created successfully',
+            'data' => new AddressCompleteResource($address)
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request, string $id)
+    {
+        $userId = $request->user()->id;
+        $address = $this->addresseService->getAddressById($userId, $id);
+        
+        return response()->json([
+            'data' => new AddressCompleteResource($address)
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAddresseRequest $request, string $id)
+    {
+        $dataValidated = $request->validated();
+        $address = $this->addresseService->updateAddress($id, $dataValidated);
+        
+        return response()->json([
+            'message' => 'Address updated successfully',
+            'data' => new AddressCompleteResource($address)
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request, string $id)
+    {
+        $userId = $request->user()->id;
+        $this->addresseService->deleteAddress($userId, $id);
+        
+        return response()->json([
+            'message' => 'Address deleted successfully'
+        ], 200);
+    }
+}
